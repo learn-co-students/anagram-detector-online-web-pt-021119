441300dcba2cf13be30fa4ed41c0dcd9d1d82c8a
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-03-16 03:00:38 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+require 'pry'
+class Anagram 
+  
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(array)
+    matches = []
+    array.each do |some_word|
+      if some_word.split("").sort == word.split("").sort
+        matches << some_word 
+      end 
+    end 
+    matches 
+  end 
+end 
