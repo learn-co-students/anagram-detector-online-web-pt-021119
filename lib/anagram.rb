db70b12872d5496f3b68d0a52e5757a756701512
Allen F Partlow <allenfp@gmail.com>
2019-03-17 01:06:30 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(work_list)
+    return_value = []
+    work_list.each { |x|
+      puts x.split.sort
+      puts @word.split.sort
+      if x.split("").sort == @word.split("").sort
+        return_value.push(x)
+      end
+      }
+    return_value
+  end
+
+end
