0fd0d4ea54d0a0d86ca9b36da30c77a281f12707
blitzparanoia <bluemilk44@gmail.com>
2019-03-19 19:43:28 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :name
+
+  def initialize(word)
+    @name = word
+  end
+
+  def match(array)
+    array.select {|x| x.split("").sort == @name.split("").sort}
+    #the select instance method, returns a new array containing all the true elements from the block
+    #note: that it handles true items unlike .select!
+  end
+
+end
