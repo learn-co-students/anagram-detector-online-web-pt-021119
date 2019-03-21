d4542a171a36eaf7224764d5a9a62aeb5f63c637
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-03-21 03:04:24 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(detecter)
+      @word = detecter
+  end 
+  
+  def match(words_array)
+    words_array.select do |word|
+      @word.split("").sort == word.split("").sort
+    end 
+  end 
+end 
