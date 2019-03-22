6f6322a0441d5d818fd70b06eff029b4e9e26b4d
Malcolm Leake <echelon1089@gmail.com>
2019-03-22 04:16:51 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,11 +4,20 @@ class Anagram
 
   def initialize(words)
   @word = words
-  binding.pry
-  
+  #binding.pry
   end 
   
-  def match(words)
+
   
+  
+  
+  def match(compare_words)
+    new_ana= []
+    compare_words.each do |comp_word|
+      if comp_word.split("").sort == @word.split("").sort
+        new_ana << comp_word
+      end 
+    end
+    new_ana
   end 
 end
